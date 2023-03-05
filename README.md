eb4245e202ca080bf76a132ad6f65a012ea7e01a
khalifa007 <moodyoo935@gmail.com>
2023-03-05 01:42:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# 🍌 Shiba Diffusion
+
+This repo gives a framework to run ckpt files on Banana's serverless GPU platform. The example model used here is NowAi's Shiba Diffusion model which can be found [here](https://huggingface.co/lucataco/Shiba)
+
+<img src="./shibaTux.jpg">
+
+
+# Quickstart
+**[Follow the quickstart guide in Banana's documentation to use this repo](https://docs.banana.dev/banana-docs/quickstart).** 
+
+*(choose "GitHub Repository" deployment method)*
+
+<br>
+
+# Helpful Links
+Understand the 🍌 [Serverless framework](https://docs.banana.dev/banana-docs/core-concepts/inference-server/serverless-framework) and functionality of each file within it.
+
+Generalize this framework to [deploy anything on Banana](https://docs.banana.dev/banana-docs/resources/how-to-serve-anything-on-banana).
+
+<br>
+
+## Use Banana for scale.
---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import torch
+import base64
+from io import BytesIO
+from diffusers import StableDiffusionPipeline
+
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
+def init():
+    global model
+    
+    # os.system("python convert_original_stable_diffusion_to_diffusers.py --checkpoint_path ./local.ckpt --dump_path sd_weights/")
+    model = StableDiffusionPipeline.from_pretrained(
+        "sd_weights/", 
+        safety_checker=None,
+        revision="fp16",
+        torch_dtype=torch.float16
+    ).to("cuda")
+
+
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
+
+    # Parse out your arguments
+    prompt = model_inputs.get('prompt', None)
+    steps = model_inputs.get('inference_steps', 50)
+    scale = model_inputs.get('guidance_scale', 5)
+    if prompt == None:
+        return {'message': "No prompt provided"}
+    
+    # Run the model
+    fullPrompt = "shbdg " + prompt
+    image = model(fullPrompt, num_inference_steps=steps, guidance_scale=scale).images[0]
+    buffered = BytesIO()
+    image.save(buffered,format="JPEG")
+    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    
+     # Return the results as a dictionary
+    return {'image_base64': image_base64}
